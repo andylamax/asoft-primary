8781c13279238f79f6ed829030f02ae802b68758
andylamax <andylamax@programmer.net>
2020-04-12 13:13:16 +0300

    Added send-grid and siltech api

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        maven(url = "https://jitpack.io")
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-js-wrappers")
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-dev")
+        maven(url = "https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+    group = "tz.co.asoft"
+    version = "1.0.0"
+}
---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,13 @@
+object versions {
+    const val kotlin = "1.4-M1"
+    const val ktor = "1.3.2-1.4-M1"
+
+    object asoft {
+        const val core = "1.0.0-alpha3"
+        const val basics = "1.0.0-alpha2"
+    }
+
+    object android {
+        const val build_tools = "3.6.0"
+    }
+}
